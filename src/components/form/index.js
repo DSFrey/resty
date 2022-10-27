ba8c53c75772068d46e7d897515cf2fea183f644
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-10-27 00:06:58 +0000

    Change to controlled form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -4,18 +4,29 @@ import './form.scss';
 export const Form = (props) => {
   const availableMethods = ['GET', 'POST', 'PUT', 'DELETE']
   const [currentMethod, setCurrentMethod] = useState('GET')
+  const [currentUrl, setCurrentUrl] = useState('')
+  const [currentData, setCurrentData] = useState('')
 
   const handleMethod = (e) => {
     setCurrentMethod(e.target.id)
+  }
+
+  const handleUrl = (e) => {
+    setCurrentUrl(e.target.value)
+  }
+
+  const handleData = (e) => {
+    setCurrentData(e.target.value)
   }
 
   const handleSubmit = e => {
     e.preventDefault();
     const formData = {
       method: currentMethod,
-      url: e.target[0].value,
-      data: e.target[2].value,
+      url: currentUrl,
+      data: currentData,
     };
+    console.log(formData)
     props.handleApiCall(formData);
   }
 
@@ -24,7 +35,7 @@ export const Form = (props) => {
       <form onSubmit={handleSubmit} data-testid='form'>
         <label >
           <span>URL: </span>
-          <input name='url' type='text' />
+          <input name='url' type='text' value={currentUrl} onChange={handleUrl}/>
           <button type="submit">GO!</button>
         </label>
         <label className="methods">
@@ -39,7 +50,7 @@ export const Form = (props) => {
         </label>
         <label>
           <span>JSON: </span>
-          <textarea name='JSON'></textarea>
+          <textarea name='JSON' value={currentData} onChange={handleData}></textarea>
         </label>
       </form>
     </>
